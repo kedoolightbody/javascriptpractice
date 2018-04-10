8c6e25d6e9d3ec935625c1ce3cacca3049eef6e6
kedoo lightbody <35748718+kedoolightbody@users.noreply.github.com>
2018-04-10 17:41:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+	$('.parallax').parallax();
+});
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+
+    /* jQuery Method Calls
+      You can still use the old jQuery plugin method calls.
+      But you won't be able to access instance properties.
+
+      $('.dropdown-trigger').dropdown('methodName');
+      $('.dropdown-trigger').dropdown('methodName', paramName);
+    */
+          
